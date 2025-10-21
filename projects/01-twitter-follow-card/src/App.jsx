fc7b54f90e605934c1b83e0474c0b6e19170bb8f
dieginin <diegobalestra9@gmail.com>
2025-10-20 22:37:14 -0700

    feat: refactor App component to use users array for rendering TwitterFollowCard

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,15 +1,28 @@
 import { TwitterFollowCard } from './components/TwitterFollowCard'
 
+const users = [
+  { userName: "midudev", name: "Miguel Angel Duran" },
+  { userName: "ConBdeBuenazo", name: "Diego Balestra", initialIsFollowing: true },
+  { userName: "elonmusk", name: "Elon Musk" },
+  { userName: "BrawlStars", name: "Brawl Stars" },
+  { userName: "BetaProfiles", name: "Beta Profiles" },
+]
+
 export function App() {
   return (
     <section className='tw-whoToFollowCard'>
       <strong className='tw-whoToFollowCard-title'>Who to follow</strong>
       <section className="tw-whoToFollowCard-suggestions">
-        <TwitterFollowCard userName="midudev" name="Miguel Angel Duran" />
-        <TwitterFollowCard userName="ConBdeBuenazo" name="Diego Balestra" initialIsFollowing />
-        <TwitterFollowCard userName="elonmusk" name="Elon Musk" />
-        <TwitterFollowCard userName="BrawlStars" name="Brawl Stars" />
-        <TwitterFollowCard userName="BetaProfiles" name="Beta Profiles" />
+        {
+          users.map(({ userName, name, initialIsFollowing }) => (
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              name={name}
+              initialIsFollowing={initialIsFollowing}
+            />
+          ))
+        }
       </section>
     </section>
   )
