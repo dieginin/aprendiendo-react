4b3a22046eb13c9a66fc059c5d97345c1b316555
dieginin <diegobalestra9@gmail.com>
2025-10-21 02:13:16 -0700

    feat: initialize project with Vite and React
    - Add package.json with scripts and dependencies for Vite and React
    - Create main application file (App.jsx) to fetch random cat facts and images
    - Add styles for the main layout and images
    - Set up Vite configuration for React
    - Include SVG icons for branding

---
+++ projects/02-prueba-tecnica/src/App.jsx
@@ -0,0 +1,37 @@
+import './style.css'
+
+import { useEffect, useState } from 'react'
+
+export default function App () {
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  const firstThreeWords = fact ? fact.split(' ', 3).join(' ') : ''
+
+  const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+  const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstThreeWords}?fontSize=50&fontColor=red&json=true`
+
+  // Obtener un dato aleatorio de gato
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
+
+  // Obtener una imagen de gato con las primeras tres palabras del dato anterior
+  useEffect(() => {
+    if (!fact) return
+
+    fetch(CAT_ENDPOINT_IMAGE_URL)
+      .then(res => res.json())
+      .then(data => setImageUrl(data.url))
+  }, [fact])
+
+  return (
+    <main>
+      <h1>App de Gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <p><img src={imageUrl} alt={`Imagen aleatoria de gato generada con las primeras tres palabras de "${fact}"`} /></p>}
+    </main>
+  )
+}
