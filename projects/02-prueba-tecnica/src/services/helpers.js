18faddcf9b22bf810217a5930c50b8df33472ca3
dieginin <diegobalestra9@gmail.com>
2025-10-21 22:04:28 -0700

    fix: remove fontColor parameter from cat image URL for consistency

--- projects/02-prueba-tecnica/src/services/helpers.js
+++ projects/02-prueba-tecnica/src/services/helpers.js
@@ -10,7 +10,7 @@ export const getRandomFact = () => {
 // Obtener una imagen de gato con las primeras tres palabras del dato anterior
 export const getCatImageFromFact = ({ fact }) => {
   const firstThreeWords = fact.split(' ', 3).join(' ')
-  const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstThreeWords}?fontSize=50&fontColor=red&json=true`
+  const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstThreeWords}?fontSize=50&json=true`
 
   return fetch(CAT_ENDPOINT_IMAGE_URL)
     .then(res => res.json())
